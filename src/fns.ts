0d6eeb5644a62608acaf68177fecc1de3930af40
Sam Gwilym <sam@gwil.garden>
2023-03-21 15:35:01 +0000

    Add completely untested encode / decode fns

--- src/fns.ts
+++ src/fns.ts
@@ -1,25 +1,84 @@
-import { AuthorKeypair, NamespaceKeypair, SignedEntry } from "./types.ts";
+import { Entry } from "./types.ts";
 
-function generateAuthorKeypair(shortname: string): AuthorKeypair;
+export function encodeEntry(
+  entry: Entry,
+): Uint8Array {
+  if (entry.identifier.path.byteLength > 256) {
+    throw new Error("Record identifier path is longer than 2048 bits");
+  }
 
-function generateNamespaceKeypair(shortname: string): NamespaceKeypair;
+  // Namespace pubkey + Author pubkey + 64 bit uint + path bytelength
+  const recordIdentifierLength = entry.identifier.namespace.byteLength +
+    entry.identifier.author.byteLength +
+    8 + entry.identifier.path.byteLength;
+  // 64 bit uint + digest
+  const recordLength = 8 + entry.record.hash.byteLength;
 
-function generateEntry(
-  opts: {
-    author: AuthorKeypair;
-    namespace: NamespaceKeypair;
-    path: Uint8Array;
-    data: Uint8Array;
-    timestamp?: Uint8Array;
-  },
-): SignedEntry;
+  const totalLength = recordIdentifierLength + recordLength;
 
-function encodeEntry(
-  signedEntry: SignedEntry,
-): Uint8Array;
+  const ui8 = new Uint8Array(totalLength);
+  const dataView = new DataView(ui8);
 
-function decodeEntry(
+  let currentPosition = 0;
+
+  // Record identifier
+
+  // Namespace pubkey
+  ui8.set(entry.identifier.namespace, currentPosition);
+
+  currentPosition += entry.identifier.namespace.byteLength;
+
+  // Author pubkey
+  ui8.set(entry.identifier.namespace, currentPosition);
+
+  currentPosition += entry.identifier.author.byteLength;
+
+  // Timestamp
+  dataView.setBigUint64(currentPosition, entry.identifier.timestamp);
+
+  currentPosition += 8;
+
+  // Path
+  ui8.set(entry.identifier.path, currentPosition);
+
+  currentPosition += entry.identifier.path.byteLength;
+
+  // Record
+
+  // Length
+  dataView.setBigUint64(currentPosition, entry.record.length);
+
+  currentPosition += 8;
+
+  // Hash
+
+  ui8.set(entry.record.hash, currentPosition);
+
+  return ui8;
+}
+
+export function decodeEntry(
   encodedEntry: Uint8Array,
-): SignedEntry;
+  opts: {
+    pubKeyLength: number;
+    digestLength: number;
+  },
+): Entry {
+  const dataView = new DataView(encodedEntry);
 
-// Not going to even think about storing entries or merging them right now.
+  const pathLength = encodedEntry.byteLength - (8 + opts.digestLength) -
+    (opts.pubKeyLength * 2 + 8);
+
+  return {
+    identifier: {
+      namespace: encodedEntry.subarray(0, opts.pubKeyLength),
+      author: encodedEntry.subarray(opts.pubKeyLength, opts.pubKeyLength),
+      timestamp: dataView.getBigUint64(opts.pubKeyLength * 2),
+      path: encodedEntry.subarray(opts.pubKeyLength * 2 + 16, pathLength),
+    },
+    record: {
+      length: dataView.getBigUint64(opts.pubKeyLength * 2 + 16 + pathLength),
+      hash: encodedEntry.subarray(opts.pubKeyLength * 2 + 16 + pathLength + 16),
+    },
+  };
+}
