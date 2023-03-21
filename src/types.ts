573bd264404a07d8b345577d43d053b1deb5a6cb
Sam Gwilym <sam@gwil.garden>
2023-03-21 15:34:40 +0000

    Remove signature + hash scheme specification

--- src/types.ts
+++ src/types.ts
@@ -1,46 +1,18 @@
-export type AuthorId = {
-  /** a sequence of four ascii characters (lower-case letters and numbers only) */
-  // TS is not adequately capable of expressing this,
-  // And JS doesn't even have chars...
-  // is this a tuple of 4 strings, or 4 numbers 0-255?
-  shortname: [string, string, string, string];
-  /** Bit string */
-  publicKey: Uint8Array;
-};
-
-export type AuthorKeypair = {
-  address: AuthorId;
-  /** Bit string */
-  secret: Uint8Array;
-};
-
-export type NamespaceId = {
-  // Should namespace 'names' have fixed length, like authors?
-  // Alternatively they are encoded with their length.
-  name: string;
-  /** Bit string */
-  publicKey: Uint8Array;
-};
-
-export type NamespaceKeypair = {
-  address: NamespaceId;
-  /** Bit string */
-  secret: Uint8Array;
-};
-
 export type RecordIdentifier = {
-  namespace: NamespaceId;
-  author: AuthorId;
+  /** The namespace's public key as a fixed-width integer */
+  namespace: Uint8Array;
+  /** The author's public key as a fixed-width integer*/
+  author: Uint8Array;
   /** 64 bit integer (interpreted as microseconds since the Unix epoch). Big-endian. */
-  timestamp: Uint8Array;
+  timestamp: bigint;
   /** Bit string of length at most 2048 */
   path: Uint8Array;
 };
 
 export type Record = {
   /** 64 bit integer */
-  length: Uint8Array;
-  /** 256 bit integer*/
+  length: bigint;
+  /** digest-length bit integer*/
   hash: Uint8Array;
 };
 
@@ -50,6 +22,7 @@ export type Entry = {
 };
 
 export type SignedEntry = {
+  recordIdentifier: RecordIdentifier;
   entry: Entry;
   /** Bit string */
   authorSignature: Uint8Array;
