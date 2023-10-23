58faa8043ad6e9d54ba885b31913dad7d6d0adc1
Sam Gwilym <sam@gwil.garden>
2023-10-23 16:42:42 +0200

    Add Storage3d get, insert, remove tests

--- src/replica/storage/storage_3d/storage_3d.test.ts
+++ src/replica/storage/storage_3d/storage_3d.test.ts
@@ -1,5 +1,5 @@
 import { assert } from "https://deno.land/std@0.202.0/assert/assert.ts";
-import { encodeBase32, encodeBase64, Products } from "../../../../deps.ts";
+import { encodeBase64, Products } from "../../../../deps.ts";
 import {
   makeNamespaceKeypair,
   makeSubspaceKeypair,
@@ -71,18 +71,51 @@ const tripleStorageScenario: Storage3dScenario<
 
 const scenarios = [tripleStorageScenario];
 
-Deno.test("entriesByQuery", async () => {
-  // Items must come in right order
-  // Must use bound correctly
-  // Must respect limit
-  // Must respect reverse
+Deno.test("Storage3d.insert, get, and remove", async (test) => {
+  for (const scenario of scenarios) {
+    const namespaceKeypair = await makeNamespaceKeypair();
 
-  // Create a replica.
+    const { storage, dispose } = await scenario.makeScenario(
+      namespaceKeypair.namespace,
+    );
 
-  // Insert a random number of entries.
+    await test.step(scenario.name, async () => {
+      const subspace = await makeSubspaceKeypair();
+      const pathAndPayload = crypto.getRandomValues(new Uint8Array(8));
 
-  // Hold onto these entries for checking presence later
+      const payloadHash = crypto.getRandomValues(new Uint8Array(32));
+      const authTokenHash = crypto.getRandomValues(new Uint8Array(32));
 
+      await storage.insert({
+        path: pathAndPayload,
+        payloadHash,
+        authTokenHash,
+        length: BigInt(8),
+        subspace: subspace.subspace,
+        timestamp: BigInt(1000),
+      });
+
+      const res = await storage.get(subspace.subspace, pathAndPayload);
+
+      assert(res);
+
+      assertEquals(res.entry.identifier.subspace, subspace.subspace);
+      assertEquals(res.entry.identifier.path, pathAndPayload);
+      assertEquals(res.entry.record.hash, payloadHash);
+      assertEquals(res.authTokenHash, authTokenHash);
+
+      await storage.remove(res.entry);
+
+      const res2 = await storage.get(subspace.subspace, pathAndPayload);
+
+      assert(res2 === undefined);
+    });
+
+    await dispose();
+  }
+});
+
+Deno.test("Storage3d.entriesByQuery", async (test) => {
   for (const scenario of scenarios) {
     const namespaceKeypair = await makeNamespaceKeypair();
 
@@ -116,262 +149,172 @@ Deno.test("entriesByQuery", async () => {
       },
     });
 
-    // Generate the test queries
+    await test.step(scenario.name, async () => {
+      // Generate the test queries
 
-    const subspaces = await getSubspaces(10);
-    const bytes = [];
+      const subspaces = await getSubspaces(10);
+      const bytes = [];
 
-    for (let i = 0; i < 50; i++) {
-      bytes.push(crypto.getRandomValues(new Uint8Array(4)));
-    }
+      for (let i = 0; i < 50; i++) {
+        bytes.push(crypto.getRandomValues(new Uint8Array(4)));
+      }
 
-    const timestamps = [];
+      const timestamps = [];
 
-    for (let i = 0; i < 25; i++) {
-      timestamps.push(randomTimestamp());
-    }
+      for (let i = 0; i < 25; i++) {
+        timestamps.push(randomTimestamp());
+      }
 
-    // Bounds
+      // Bounds
 
-    const subspaceBounds = manyRandomBounds(
-      100,
-      subspaces.map((s) => s.subspace),
-      compareBytes,
-    );
-    const pathBounds = manyRandomBounds(100, bytes, Products.orderPaths);
-    const timeBounds = manyRandomBounds(
-      100,
-      timestamps,
-      Products.orderTimestamps,
-    );
+      const subspaceBounds = manyRandomBounds(
+        100,
+        subspaces.map((s) => s.subspace),
+        compareBytes,
+      );
+      const pathBounds = manyRandomBounds(100, bytes, Products.orderPaths);
+      const timeBounds = manyRandomBounds(
+        100,
+        timestamps,
+        Products.orderTimestamps,
+      );
 
-    const queries: Query<Uint8Array>[] = [];
+      const queries: Query<Uint8Array>[] = [];
 
-    const includedByQueries = (
-      subspace: Uint8Array,
-      path: Uint8Array,
-      time: bigint,
-    ): Query<Uint8Array>[] => {
-      const includedQueries = [];
+      const includedByQueries = (
+        subspace: Uint8Array,
+        path: Uint8Array,
+        time: bigint,
+      ): Query<Uint8Array>[] => {
+        const includedQueries = [];
+
+        for (const query of queries) {
+          if (query.subspace) {
+            const range = rangeFromOptionalBounds(query.subspace, emptyUi8);
+
+            const isIncluded = Products.rangeIncludesValue(
+              { order: compareBytes },
+              range,
+              subspace,
+            );
+
+            if (!isIncluded) {
+              continue;
+            }
+          }
+
+          if (query.path) {
+            const range = rangeFromOptionalBounds(query.path, emptyUi8);
+
+            const isIncluded = Products.rangeIncludesValue(
+              { order: Products.orderPaths },
+              range,
+              path,
+            );
+
+            if (!isIncluded) {
+              continue;
+            }
+          }
+
+          if (query.time) {
+            const range = rangeFromOptionalBounds(query.time, BigInt(0));
+
+            const isIncluded = Products.rangeIncludesValue(
+              { order: Products.orderTimestamps },
+              range,
+              time,
+            );
+
+            if (!isIncluded) {
+              continue;
+            }
+          }
+
+          includedQueries.push(query);
+        }
+
+        return includedQueries;
+      };
+
+      for (let i = 0; i < 500; i++) {
+        const orderRoll = Math.random();
+
+        const query: Query<Uint8Array> = {
+          limit: Math.random() < 0.1
+            ? Math.floor(Math.random() * 10)
+            : undefined,
+          reverse: Math.random() < 0.25 ? true : false,
+          order: orderRoll < 0.33
+            ? "subspace"
+            : orderRoll < 0.66
+            ? "path"
+            : "timestamp",
+          subspace: Math.random() < 0.5 ? sample(subspaceBounds) : undefined,
+          path: Math.random() < 0.5 ? sample(pathBounds) : undefined,
+          time: Math.random() < 0.5 ? sample(timeBounds) : undefined,
+        };
+
+        queries.push(query);
+      }
+
+      const queryInclusionMap = new Map<
+        Query<Uint8Array>,
+        Set<string>
+      >();
 
       for (const query of queries) {
-        if (query.subspace) {
-          const range = rangeFromOptionalBounds(query.subspace, emptyUi8);
-
-          const isIncluded = Products.rangeIncludesValue(
-            { order: compareBytes },
-            range,
-            subspace,
-          );
-
-          if (!isIncluded) {
-            continue;
-          }
-        }
-
-        if (query.path) {
-          const range = rangeFromOptionalBounds(query.path, emptyUi8);
-
-          const isIncluded = Products.rangeIncludesValue(
-            { order: Products.orderPaths },
-            range,
-            path,
-          );
-
-          if (!isIncluded) {
-            continue;
-          }
-        }
-
-        if (query.time) {
-          const range = rangeFromOptionalBounds(query.time, BigInt(0));
-
-          const isIncluded = Products.rangeIncludesValue(
-            { order: Products.orderTimestamps },
-            range,
-            time,
-          );
-
-          if (!isIncluded) {
-            continue;
-          }
-        }
-
-        includedQueries.push(query);
+        queryInclusionMap.set(query, new Set());
       }
 
-      return includedQueries;
-    };
+      replica.addEventListener("entryremove", (event) => {
+        const { detail: { removed } } = event as CustomEvent<
+          { removed: Entry<Uint8Array, Uint8Array, Uint8Array> }
+        >;
 
-    for (let i = 0; i < 500; i++) {
-      const orderRoll = Math.random();
+        const encodedEntry = encodeEntry(removed, {
+          namespaceScheme: testSchemeNamespace,
+          subspaceScheme: testSchemeSubspace,
+          pathLengthScheme: testSchemePathLength,
+          payloadScheme: testSchemePayload,
+        });
 
-      const query: Query<Uint8Array> = {
-        limit: Math.random() < 0.1 ? Math.floor(Math.random() * 10) : undefined,
-        reverse: Math.random() < 0.25 ? true : false,
-        order: orderRoll < 0.33
-          ? "subspace"
-          : orderRoll < 0.66
-          ? "path"
-          : "timestamp",
-        subspace: Math.random() < 0.5 ? sample(subspaceBounds) : undefined,
-        path: Math.random() < 0.5 ? sample(pathBounds) : undefined,
-        time: Math.random() < 0.5 ? sample(timeBounds) : undefined,
-      };
+        testSchemePayload.fromBytes(encodedEntry).then((hash) => {
+          const b64 = encodeBase64(hash);
 
-      queries.push(query);
-    }
-
-    const queryInclusionMap = new Map<
-      Query<Uint8Array>,
-      Set<string>
-    >();
-
-    for (const query of queries) {
-      queryInclusionMap.set(query, new Set());
-    }
-
-    replica.addEventListener("entryremove", (event) => {
-      const { detail: { removed } } = event as CustomEvent<
-        { removed: Entry<Uint8Array, Uint8Array, Uint8Array> }
-      >;
-
-      const encodedEntry = encodeEntry(removed, {
-        namespaceScheme: testSchemeNamespace,
-        subspaceScheme: testSchemeSubspace,
-        pathLengthScheme: testSchemePathLength,
-        payloadScheme: testSchemePayload,
+          for (const [, set] of queryInclusionMap) {
+            set.delete(b64);
+          }
+        });
       });
 
-      testSchemePayload.fromBytes(encodedEntry).then((hash) => {
-        const b64 = encodeBase64(hash);
+      const entryAuthHashMap = new Map<string, string>();
 
-        for (const [, set] of queryInclusionMap) {
-          set.delete(b64);
+      // Generate the entries
+      for (let i = 0; i < 50; i++) {
+        const pathAndPayload = sample(bytes)!;
+        const chosenSubspace = sample(subspaces)!;
+        const timestamp = sample(timestamps)!;
+
+        const result = await replica.set({
+          path: pathAndPayload,
+          payload: pathAndPayload,
+          subspace: chosenSubspace.subspace,
+          timestamp,
+        }, chosenSubspace.privateKey);
+
+        if (result.kind !== "success") {
+          continue;
         }
-      });
-    });
 
-    const entryAuthHashMap = new Map<string, string>();
+        // See if it belongs to any of the test queries.
+        const correspondingQueries = includedByQueries(
+          chosenSubspace.subspace,
+          pathAndPayload,
+          timestamp,
+        );
 
-    // Generate the entries
-    for (let i = 0; i < 50; i++) {
-      const pathAndPayload = sample(bytes)!;
-      const chosenSubspace = sample(subspaces)!;
-      const timestamp = sample(timestamps)!;
-
-      const result = await replica.set({
-        path: pathAndPayload,
-        payload: pathAndPayload,
-        subspace: chosenSubspace.subspace,
-        timestamp,
-      }, chosenSubspace.privateKey);
-
-      if (result.kind !== "success") {
-        continue;
-      }
-
-      // See if it belongs to any of the test queries.
-      const correspondingQueries = includedByQueries(
-        chosenSubspace.subspace,
-        pathAndPayload,
-        timestamp,
-      );
-
-      const encodedEntry = encodeEntry(result.entry, {
-        namespaceScheme: testSchemeNamespace,
-        subspaceScheme: testSchemeSubspace,
-        pathLengthScheme: testSchemePathLength,
-        payloadScheme: testSchemePayload,
-      });
-
-      const entryHash = await testSchemePayload.fromBytes(encodedEntry);
-
-      const b64EntryHash = encodeBase64(entryHash);
-
-      // We'll use a base64 encoding of the entry's signature as an ID
-      const authTokenHash = await testSchemePayload.fromBytes(
-        new Uint8Array(result.authToken),
-      );
-      const b64AuthHash = encodeBase64(authTokenHash);
-
-      entryAuthHashMap.set(b64EntryHash, b64AuthHash);
-
-      for (const query of correspondingQueries) {
-        const set = queryInclusionMap.get(query)!;
-
-        set.add(b64EntryHash);
-      }
-    }
-
-    for (const query of queries) {
-      let entriesRead = 0;
-
-      const awaiting = new Set(queryInclusionMap.get(query));
-
-      const prevIsCorrectOrder = (
-        prev: Entry<Uint8Array, Uint8Array, ArrayBuffer>,
-        curr: Entry<Uint8Array, Uint8Array, ArrayBuffer>,
-        ord: "path" | "timestamp" | "subspace",
-      ): boolean => {
-        switch (ord) {
-          case "path": {
-            const order = compareBytes(
-              prev.identifier.path,
-              curr.identifier.path,
-            );
-
-            if (order === 0) {
-              return prevIsCorrectOrder(prev, curr, "timestamp");
-            }
-
-            if (query.reverse) {
-              return order === 1;
-            }
-
-            return order === -1;
-          }
-          case "timestamp": {
-            const order = Products.orderTimestamps(
-              prev.record.timestamp,
-              curr.record.timestamp,
-            );
-
-            if (order === 0) {
-              return prevIsCorrectOrder(prev, curr, "subspace");
-            }
-
-            if (query.reverse) {
-              return order === 1;
-            }
-
-            return order === -1;
-          }
-          case "subspace": {
-            const order = compareBytes(
-              prev.identifier.subspace,
-              curr.identifier.subspace,
-            );
-
-            if (order === 0) {
-              return prevIsCorrectOrder(prev, curr, "path");
-            }
-
-            if (query.reverse) {
-              return order === 1;
-            }
-
-            return order === -1;
-          }
-        }
-      };
-
-      let prevEntry: Entry<Uint8Array, Uint8Array, ArrayBuffer> | undefined;
-
-      for await (
-        const { entry, authTokenHash } of storage.entriesByQuery(query)
-      ) {
-        const encodedEntry = encodeEntry(entry, {
+        const encodedEntry = encodeEntry(result.entry, {
           namespaceScheme: testSchemeNamespace,
           subspaceScheme: testSchemeSubspace,
           pathLengthScheme: testSchemePathLength,
@@ -383,35 +326,131 @@ Deno.test("entriesByQuery", async () => {
         const b64EntryHash = encodeBase64(entryHash);
 
         // We'll use a base64 encoding of the entry's signature as an ID
+        const authTokenHash = await testSchemePayload.fromBytes(
+          new Uint8Array(result.authToken),
+        );
         const b64AuthHash = encodeBase64(authTokenHash);
 
-        assertEquals(entryAuthHashMap.get(b64EntryHash), b64AuthHash);
+        entryAuthHashMap.set(b64EntryHash, b64AuthHash);
 
-        // Test order
-        if (prevEntry) {
-          assert(prevIsCorrectOrder(prevEntry, entry, query.order));
-        }
+        for (const query of correspondingQueries) {
+          const set = queryInclusionMap.get(query)!;
 
-        assert(queryInclusionMap.get(query)?.has(b64EntryHash));
-        entriesRead += 1;
-        prevEntry = entry;
-
-        awaiting.delete(b64EntryHash);
-
-        if (query.limit && entriesRead > query.limit) {
-          assert(false, "Too many entries received for query");
+          set.add(b64EntryHash);
         }
       }
 
-      if (query.limit) {
-        assertEquals(
-          entriesRead,
-          Math.min(query.limit, queryInclusionMap.get(query)!.size),
-        );
-      } else {
-        assertEquals(entriesRead, queryInclusionMap.get(query)!.size);
+      for (const query of queries) {
+        let entriesRead = 0;
+
+        const awaiting = new Set(queryInclusionMap.get(query));
+
+        const prevIsCorrectOrder = (
+          prev: Entry<Uint8Array, Uint8Array, ArrayBuffer>,
+          curr: Entry<Uint8Array, Uint8Array, ArrayBuffer>,
+          ord: "path" | "timestamp" | "subspace",
+        ): boolean => {
+          switch (ord) {
+            case "path": {
+              const order = compareBytes(
+                prev.identifier.path,
+                curr.identifier.path,
+              );
+
+              if (order === 0) {
+                return prevIsCorrectOrder(prev, curr, "timestamp");
+              }
+
+              if (query.reverse) {
+                return order === 1;
+              }
+
+              return order === -1;
+            }
+            case "timestamp": {
+              const order = Products.orderTimestamps(
+                prev.record.timestamp,
+                curr.record.timestamp,
+              );
+
+              if (order === 0) {
+                return prevIsCorrectOrder(prev, curr, "subspace");
+              }
+
+              if (query.reverse) {
+                return order === 1;
+              }
+
+              return order === -1;
+            }
+            case "subspace": {
+              const order = compareBytes(
+                prev.identifier.subspace,
+                curr.identifier.subspace,
+              );
+
+              if (order === 0) {
+                return prevIsCorrectOrder(prev, curr, "path");
+              }
+
+              if (query.reverse) {
+                return order === 1;
+              }
+
+              return order === -1;
+            }
+          }
+        };
+
+        let prevEntry: Entry<Uint8Array, Uint8Array, ArrayBuffer> | undefined;
+
+        for await (
+          const { entry, authTokenHash } of storage.entriesByQuery(query)
+        ) {
+          const encodedEntry = encodeEntry(entry, {
+            namespaceScheme: testSchemeNamespace,
+            subspaceScheme: testSchemeSubspace,
+            pathLengthScheme: testSchemePathLength,
+            payloadScheme: testSchemePayload,
+          });
+
+          const entryHash = await testSchemePayload.fromBytes(encodedEntry);
+
+          const b64EntryHash = encodeBase64(entryHash);
+
+          // We'll use a base64 encoding of the entry's signature as an ID
+          const b64AuthHash = encodeBase64(authTokenHash);
+
+          assertEquals(entryAuthHashMap.get(b64EntryHash), b64AuthHash);
+
+          // Test order
+          if (prevEntry) {
+            assert(prevIsCorrectOrder(prevEntry, entry, query.order));
+          }
+
+          assert(queryInclusionMap.get(query)?.has(b64EntryHash));
+          entriesRead += 1;
+          prevEntry = entry;
+
+          awaiting.delete(b64EntryHash);
+
+          if (query.limit && entriesRead > query.limit) {
+            assert(false, "Too many entries received for query");
+          }
+        }
+
+        if (query.limit) {
+          assertEquals(
+            entriesRead,
+            Math.min(query.limit, queryInclusionMap.get(query)!.size),
+          );
+        } else {
+          assertEquals(entriesRead, queryInclusionMap.get(query)!.size);
+        }
       }
-    }
+    });
+
+    await dispose();
   }
 });
 
