22c384fde6d10357f28e6d5750cae08e3c9cfd78
Zicklag <zicklag@katharostech.com>
2024-12-26 20:08:10 -0600

    fix: fix web bundle script and compile typescript definitions.

--- scripts/build_web_bundle.ts
+++ scripts/build_web_bundle.ts
@@ -1,11 +1,20 @@
 import * as esbuild from "npm:esbuild";
-import { denoPlugins } from "@luca/esbuild-deno-loader";
+import {
+  denoPlugins,
+  denoResolverPlugin,
+  denoLoaderPlugin,
+} from "@luca/esbuild-deno-loader";
+
+import ts from "npm:typescript@5.7.2";
 
 const version = Deno.args[0];
 
+const cwd = Deno.cwd();
+
 const result = await esbuild.build({
   plugins: [
-    ...denoPlugins(),
+    denoResolverPlugin({ configPath: `/${cwd}/deno.json` }),
+    denoLoaderPlugin({ configPath: `/${cwd}/deno.json` }),
   ],
   entryPoints: ["./mod.browser-bundle.ts"],
   outfile: `./dist/willow${version ? `-${version}` : ""}.web.js`,
@@ -20,8 +29,19 @@ const result = await esbuild.build({
 if (result.metafile) {
   await Deno.writeTextFile(
     "./dist/metafile.json",
-    JSON.stringify(result.metafile),
+    JSON.stringify(result.metafile)
   );
 }
+
+const program = ts.createProgram({
+  rootNames: ["./mod.browser-bundle.ts"],
+  options: {
+    declaration: true,
+    emitDeclarationOnly: true,
+    outFile: "./dist/index.d.ts",
+  },
+});
+
+program.emit();
 
 Deno.exit(0);
